60cfdce76b1c114fac46418a276a9b850a3af944
kereeh <kereeh@Kereeh.localdomain>
2017-06-16 14:34:32 -0400

    "update modified files"

--- week1/citibike.R
+++ week1/citibike.R
@@ -17,34 +17,60 @@ names(trips) <- gsub(' ', '_', names(trips))
 # recode gender as a factor 0->"Unknown", 1->"Male", 2->"Female"
 trips <- mutate(trips, gender = factor(gender, levels=c(0,1,2), labels = c("Unknown","Male","Female")))
 
+#convert birthyear type from character to string
+trips <- mutate(trips, birth_year = as.numeric(birth_year))
 
 ########################################
 # YOUR SOLUTIONS BELOW
 ########################################
 
 # count the number of trips (= rows in the data frame)
+nrow(trips)
 
 # find the earliest and latest birth years (see help for max and min to deal with NAs)
+summarize(trips)
+max(trips[,"birth_year"], na.rm=T)
+min(trips[,"birth_year"], na.rm=T)
 
 # use filter and grepl to find all trips that either start or end on broadway
+(filter(trips, grepl('Broadway', start_station_name) | grepl('Broadway', end_station_name)))
 
 # do the same, but find all trips that both start and end on broadway
+(filter(trips, grepl('Broadway', start_station_name) , grepl('Broadway', end_station_name)))
 
 # find all unique station names
+unique(combine(trips[,"start_station_name"],trips[,"end_station_name"]))
 
 # count the number of trips by gender
+trips %>% group_by(gender) %>% summarize(count = n())
 
 # compute the average trip time by gender
+trips %>% group_by(gender) %>% summarize(mean_duration = mean(tripduration) /60)
+
 # comment on whether there's a (statistically) significant difference
+trips %>% group_by(gender) %>% summarize(sd_duration = sd(tripduration) /60)
 
 # find the 10 most frequent station-to-station trips
+trips %>% group_by(start_station_name, end_station_name) %>% summarize(count = n()) %>% arrange(desc(count))
+t <- count(trips, start_station_name, end_station_name, sort=TRUE)
+t[1:10,]
 
 # find the top 3 end stations for trips starting from each start station
+trips %>% group_by(start_station_name, end_station_name) %>% summarize(count = n()) %>% arrange(desc(count)) %>% arrange(start_station_name) %>% top_n(3) %>% View
+
 
 # find the top 3 most common station-to-station trips by gender
+trips %>%
+  group_by(gender, start_station_name, end_station_name) %>%
+  summarize(count = n()) %>%
+  arrange(-count) %>%
+  group_by(gender) %>%
+  top_n(3) %>%
+  View
 
 # find the day with the most trips
 # tip: first add a column for year/month/day without time of day (use as.Date or floor_date from the lubridate package)
-
+trips<-trips%>%mutate(ymd = as.Date(starttime)) %>% View
+trips %>% group_by(ymd) 
 # compute the average number of trips taken during each of the 24 hours of the day across the entire month
 # what time(s) of day tend to be peak hour(s)?
--- week1/plot_trips.R
+++ week1/plot_trips.R
@@ -12,30 +12,80 @@ theme_set(theme_bw())
 # load RData file output by load_trips.R
 load('trips.RData')
 
-
 ########################################
 # plot trip data
 ########################################
 
+trips %>%
+  summarise(quant = quantile(tripduration, prob=0.99)) %>%
+  View
+
 # plot the distribution of trip times across all rides
+ggplot(trips, aes(x=tripduration)) +
+  geom_histogram(position = "identity") +
+  scale_y_continuous(label=comma) +
+  scale_x_log10(label=comma)
 
 # plot the distribution of trip times by rider type
-
+ggplot(trips, aes(x=tripduration, color=usertype)) +
+  geom_histogram(position = "identity", alpha = 0.2) +
+  scale_y_continuous(label=comma) +
+  scale_x_log10(label=comma)
+  
 # plot the number of trips over each day
+trips %>% group_by(ymd) %>% 
+  summarize(count = n()) %>%
+  ggplot(aes(x = ymd, y = count)) + 
+  geom_line()
+
+trips <- mutate(trips, age = 2017-birth_year)
 
 # plot the number of trips by gender and age
-
+trips %>% 
+  group_by(gender, age) %>%
+  summarize(count=n()) %>%
+  ggplot(aes(x = age, color= gender, y = count)) + 
+  geom_point() +
+  scale_y_continuous(label=comma) +
+  ylim(0,250000) + 
+  geom_smooth(fill = NA)
+  
 # plot the ratio of male to female trips by age
 # hint: use the spread() function to reshape things to make it easier to compute this ratio
+trips %>% group_by(gender, age) %>% 
+  filter(gender!="Unknown") %>% 
+  summarize(count=n()) %>% 
+  spread(gender, count) %>% 
+  mutate(ratio = Male/Female) %>%
+  ggplot(aes(x=age, y=ratio, size=(Male+Female))) +
+  xlim(15,70) +
+  ylim(0,7.5) +
+  geom_point() 
+
 
 ########################################
 # plot weather data
 ########################################
 # plot the minimum temperature over each day
+weather %>%
+  ggplot(aes(x=ymd, y=tmin)) +
+  geom_point()
 
 # plot the minimum temperature and maximum temperature over each day
 # hint: try using the gather() function for this to reshape things before plotting
+weather %>% View
+  select(tmin,tmax,ymd) %>% 
+  gather("range","temp",1:2) %>% 
+  ggplot(aes(x=ymd, y=temp, color=range)) +
+  geom_point()
 
+#ribbon type of thing  
+weather %>%
+  ggplot(aes(x=ymd)) +
+  geom_ribbon(aes(ymin=tmin, ymax=tmax), alpha=0.2) +
+  geom_point(data = weather, aes(x=ymd, y=tmin, color="min temperature")) +
+  geom_point(data = weather, aes(x=ymd, y=tmax, color ="max temperature"))
+    
 ########################################
 # plot trip and weather data
 ########################################
@@ -43,18 +93,40 @@ load('trips.RData')
 # join trips and weather
 trips_with_weather <- inner_join(trips, weather, by="ymd")
 
-# plot the number of trips as a function of the minimum temperature, where each point represents a day
+# plot the number of trips as a function of the minimum temperature, 
+# where each point represents a day
 # you'll need to summarize the trips and join to the weather data to do this
+stuff <- trips %>% group_by(ymd) %>% summarize(count=n()) 
+stuff2 <- inner_join(stuff, weather, by = "ymd") 
+stuff2 %>%
+  ggplot(aes(x=tmin, y=count, color=ymd)) +
+  geom_point()
 
+
+trips_with_weather %>%
+  group_by(tmin,ymd) %>%
+  summarise(count=n()) %>%
+  ggplot(aes(x=tmin, y=count)) +
+    geom_point()
+  
 # repeat this, splitting results by whether there was substantial precipitation or not
-# you'll need to decide what constitutes "substantial precipitation" and create a new T/F column to indicate this
+# you'll need to decide what constitutes "substantial precipitation" and create a new 
+# T/F column to indicate this
+trips_with_weather %>%
+  mutate(subs = prcp >=quantile(prcp,0.8)) %>%
+  group_by(tmin,ymd,subs) %>%
+  summarise(count=n()) %>%
+  ggplot(aes(x=tmin, y=count, color=subs)) +
+  geom_point() +
+  geom_smooth()
 
 # add a smoothed fit on top of the previous plot, using geom_smooth
 
-# compute the average number of trips and standard deviation in number of trips by hour of the day
+# compute the average number of trips and standard deviation in 
+# number of trips by hour of the day
 # hint: use the hour() function from the lubridate package
 
 # plot the above
 
-# repeat this, but now split the results by day of the week (Monday, Tuesday, ...) or weekday vs. weekend days
+# repeat this, but now split the results by day of the week or weekday vs. weekend days
 # hint: use the wday() function from the lubridate package
